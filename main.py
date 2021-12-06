c4a3e892a455ff6cbcdf7859614695eac3f143f4
DucNguyen <54398319+ducnguyen8808@users.noreply.github.com>
2021-12-06 13:02:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from selenium import webdriver
 chrome_options = webdriver.ChromeOptions()
+prefs = {'profile.default_content_setting_values': {'cookies': 2, 'images': 2, 'javascript': 2, 
+                            'plugins': 2, 'popups': 2, 'geolocation': 2, 
+                            'notifications': 2, 'auto_select_certificate': 2, 'fullscreen': 2, 
+                            'mouselock': 2, 'mixed_script': 2, 'media_stream': 2, 
+                            'media_stream_mic': 2, 'media_stream_camera': 2, 'protocol_handlers': 2, 
+                            'ppapi_broker': 2, 'automatic_downloads': 2, 'midi_sysex': 2, 
+                            'push_messaging': 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop': 2, 
+                            'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement': 2, 
+                            'durable_storage': 2}}
+chrome_options.add_experimental_option('prefs', prefs)
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument("start-maximized")
+chrome_options.add_argument("disable-infobars")
+chrome_options.add_argument("--disable-extensions")
 
 import time
 from selenium.webdriver.common.keys import Keys
@@ -80,7 +93,6 @@ if __name__ == '__main__':
         list_proxy.remove(PROXY)
         chrome_options.add_argument('--proxy-server=%s' % PROXY)
         driver = webdriver.Chrome(PATH,chrome_options=chrome_options)
-        driver.maximize_window()
 
         crawlID(url[i * 100: (i + 1) * 100], driver, i * 100)
         driver.close()
